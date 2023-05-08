5d665853fe368f8be7f35a01214953271dfaba7e
Jalaj Goswami <jalajgoswami33@gmail.com>
2023-05-08 19:04:04 +0530

    added route for check-user-name api

--- routes/user.ts
+++ routes/user.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express'
 import {
+    checkUserName,
     createProfile
 } from '../controllers/user'
 
 const router = Router()
 
 router.post('/create-profile', createProfile)
+router.get('/check-user-name', checkUserName)
 
 
 export default router
